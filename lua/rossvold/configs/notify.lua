cbe8f5d8a47db5af4b7f17a14cf8d50946901a05
rossvold <rubenrossvold@icloud.com>
2025-05-24 12:01:22 +0200

    fix annoying warning

--- lua/rossvold/configs/notify.lua
+++ lua/rossvold/configs/notify.lua
@@ -1,15 +1,15 @@
 require("notify").setup({
 	timeout = 50,
 	max_width = 45,
-	background_colour = "NotifyBackground",
+	background_colour = "#000000",
 	fps = 30,
 	render = "wrapped-compact",
 	stages = "fade_in_slide_out",
 	time_formats = {
 		notification = "%T",
-		notification_history = "%FT%T"
+		notification_history = "%FT%T",
 	},
-	top_down = true
+	top_down = true,
 })
 
 vim.notify = require("notify")

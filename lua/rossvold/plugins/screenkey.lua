67622948af9375d332535c8829ecd2b476f61e98
rossvold <rubenrossvold@icloud.com>
2026-03-26 13:55:06 +0100

    rm screenkey

--- lua/rossvold/plugins/screenkey.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"NStefan002/screenkey.nvim",
-	lazy = false,
-	version = "*", -- or branch = "dev", to use the latest commit
-	config = function()
-		require("rossvold.configs.screenkey")
-	end,
-}

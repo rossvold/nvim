d2e0d708fb3494620c24270b34836688013f99a1
rossvold <rubenrossvold@icloud.com>
2026-04-09 10:23:19 +0200

    work

---
+++ lua/rossvold/plugins/indent.lua
@@ -0,0 +1,61 @@
+return {
+	"lukas-reineke/indent-blankline.nvim",
+	main = "ibl",
+	---@module "ibl"
+	---@type ibl.config
+	opts = {
+		indent = {
+			char = "│",
+			tab_char = "│",
+		},
+		whitespace = {
+			remove_blankline_trail = true,
+		},
+		scope = {
+			enabled = true,
+			char = "│",
+			show_start = true,
+			show_end = true,
+			include = {
+				node_type = {
+					["*"] = {
+						"function",
+						"function_definition",
+						"method",
+						"method_definition",
+						"class",
+						"class_definition",
+						"if_statement",
+						"for_statement",
+						"while_statement",
+						"try_statement",
+						"with_statement",
+						"switch_statement",
+						"case_statement",
+					},
+				},
+			},
+		},
+		exclude = {
+			filetypes = {
+				"help",
+				"terminal",
+				"lazy",
+				"lspinfo",
+				"TelescopePrompt",
+				"TelescopeResults",
+				"mason",
+				"nvdash",
+				"nvcheatsheet",
+				"dashboard",
+				"alpha",
+				"",
+			},
+			buftypes = {
+				"terminal",
+				"nofile",
+				"prompt",
+			},
+		},
+	},
+}
